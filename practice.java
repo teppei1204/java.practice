3a3edc49281042fb2609e277b0eca4fe56ee6822
teppei1204 <m-n-m09@docomo.ne.jp>
2025-02-27 10:16:18 +0900

    演算子

--- practice.java
+++ practice.java
@@ -317,3 +317,49 @@ class ScoreArray {
     System.out.println("4人目は" + data[3] + "点");
   }
 }
+
+
+## 演算子
+
+### ① 算術演算子
+- `7 / 2 -> 3`  
+  あまりは切り捨てられます。
+- `7 % 2 -> 1`  
+  余りを求める演算子です。
+
+### ② 代入・複合代入演算子
+- `a += b -> aにbを加えてaに代入`  
+  これは `a = a + b` と同じです。
+
+### ③ インクリメント・デクリメント演算子
+- `a++ -> aに1を加える`  
+  これは `a = a + 1` または `a += 1` と同じです。
+
+### ④ 関係演算子
+- `a >= 0 -> aが0以上ならtrue`
+
+### ⑤ 論理演算子
+- `(a >= 0) && (b >= 0) -> aが0以上かつbが0以上ならtrue`
+
+### Integer.parseInt(): 文字列型をint型に変換
+- `int a = Integer.parseInt(args[0]);`
+
+### コード例
+
+```java
+class Operator {
+  public static void main(String[] args) {
+    // コマンドラインから入力したデータ(文字列型)をint型に変換する
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+
+    System.out.println("a = " + a + ", b = " + b);
+    System.out.println("a + b = " + (a + b));  // 計算結果の表示
+    System.out.println("a - b = " + (a - b));  // 計算結果の表示
+    System.out.println("a * b = " + (a * b));  // 計算結果の表示
+    System.out.println("a / b = " + (a / b));  // 計算結果の表示
+    System.out.println("a % b = " + (a % b));  // 計算結果の表示
+    System.out.println("a >= 0 : " + (a >= 0));  // aが0以上かどうか
+    System.out.println("(a >= 0) && (b >= 0) : " + ((a >= 0) && (b >= 0)));  // aとbが0以上かどうか
+  }
+}
