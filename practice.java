6bef79e97af15a570d3a2c9dd6e081323395766c
teppei1204 <m-n-m09@docomo.ne.jp>
2025-03-10 12:29:32 +0900

    演習問題

--- practice.java
+++ practice.java
@@ -941,3 +941,64 @@ class StuSample {
 蕪木さん
 英語 90点・数学 80点
 平均 85.0点
+
+
+## 演習問題
+
+### `Car` クラス
+
+```java
+class Car {
+  // データ型と変数名
+  int no;
+  int speed;
+
+  // 戻り値の型、メソッド名、引数
+  void setNo(int n) {
+    no = n;
+  }
+
+  void run(int s) {
+    speed = s;
+  }
+
+  void stop() {
+    speed = 0;
+  }
+
+  void display() {
+    System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+  }
+}
+Carクラスでは、車のナンバー(no)と速度(speed)を管理し、速度の設定、停止、表示を行うメソッドを定義しています。
+
+DriveCar クラス
+java
+コピーする
+class DriveCar {
+  public static void main(String[] args) {
+    // Carクラスのオブジェクトc1を生成
+    Car c1 = new Car();
+
+    // c1のナンバーを2525に設定
+    c1.setNo(2525);
+
+    // c1の速度を30に設定
+    c1.run(30);
+
+    // c1のナンバー、速度を表示
+    c1.display();
+
+    // c1の速度を0に設定
+    c1.stop();
+
+    // c1のナンバー、速度を表示
+    c1.display();
+  }
+}
+実行結果
+実行結果として、以下のように表示されます。
+
+コピーする
+ナンバー2525の速度は30です
+ナンバー2525の速度は0です
